1e8a3dcadfc70ff46862b4f7ab3405f2675e16c8
steveraaen <sraaen@gmail.com>
2017-07-16 01:34:02 -0400

    flooded Leaflt component with ridiculous props (shit vs fan)

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -5,13 +5,20 @@ import GeoJSON from './leafMap.js'
 import helpers from './utils/helpers.js'
 import './App.css';
 
-var newArr = []
+var posArr = [];
+var childArr = [];
+var featureArr = [];
+var geoKeyArr = [];
+
 class App extends Component {
 	constructor(props) {
 		super(props);
     this.state = {
+      coordinates: null,
+      children: null,
       data: null,
-      hood: null
+      hood: null,
+      geoKey: null
     }
   }
 
@@ -25,30 +32,56 @@ class App extends Component {
       }
     }.bind(this))
 
-    function Geometry(coordinates) {
-
+    function MakePos(coordinates) {
       this.coordinates = coordinates;
-
-
+    }
+    function MakeChild(children) {
+      this.children = children;
+    }
+   function MakeGeoKey(children) {
+      this.children = children;
+    }
+    function FeatureCollection(position, children, geoKey) {
+      this.position = position;
+      this.children = children;
+      this.json = position;
+      this.geoKey = geoKey;
     }
 
     helpers.initGeoData().then(function(res) {
       if (res !== this.state.data) {
         for(let i=0; i < res.data.length; i++) {
-        var coordinates = res.data[i].geometry.coordinates;
-
-        var geometry = new Geometry(coordinates);
-         newArr.push(coordinates)
+        var position = res.data[i].geometry.coordinates;
+        var children = res.data[i].properties.T;
+        var geoKey = 'k_' + [i];
+        var pos = new MakePos(position);
+        var child = new MakeChild(children);
+        var feature = new FeatureCollection(position, children);
+        var geoKey = new MakeGeoKey(geoKey);
+         posArr.push(pos);
+         childArr.push(child);
+         featureArr.push(feature);
+         geoKeyArr.push(geoKey);
         }
-        this.setState({ data: newArr });
+       
+
+
+        console.log(posArr)
+        this.setState({ json: posArr,
+                        children: childArr,
+                        data: featureArr,
+                        geoKey: geoKeyArr,
+                        position: posArr
+                         });
       }
     }.bind(this));
 }	
   render() {
+    console.log(this.state.data)
     return (
       <div className="App">
        
-        <LeafMap  data={newArr} hood={this.state.hood} />
+        <LeafMap data={this.state.json} markers={this.state.data} children={this.state.children} position={this.state.position} hood={this.state.hood} geoKey={this.state.geoKey} />
         
       </div>
     );
--- view/src/components/leafMap.js
+++ view/src/components/leafMap.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Map, TileLayer, Marker, Popup,  GeoJSON, PropTypes as MapPropTypes } from 'react-leaflet'
-
+import { Map, TileLayer, Marker, Popup, GeoJSON, PropTypes as MapPropTypes} from 'react-leaflet'
+import L from 'leaflet'
+  var map = Map
+console.log(map)
 
 export default class LeafMap extends Component {
   constructor(props) {
@@ -12,9 +14,16 @@ export default class LeafMap extends Component {
   }
 }
 
+
+componentDidMount() {
+
+  }
+
+
+
   render() {
 
-console.log(this.props.data)
+
     function getStyle(feature, layer) {
      return {
        color: '#006400',
@@ -23,6 +32,7 @@ console.log(this.props.data)
      }
    }
     const center = [this.state.lat, this.state.lng];
+  console.log(this.props)
     return (
 
       <Map center={center} zoom={this.state.zoom}>      
@@ -30,9 +40,10 @@ console.log(this.props.data)
 
           attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           url="https://api.mapbox.com/styles/v1/sraaen/cj52ii4g62aqy2so4s6zbl9g9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3JhYWVuIiwiYSI6ImNqMmt2Y3k4djAwNGczM3IzaWU1a3E1eW8ifQ.dTGNBuW1jqOckGIAEDOUZw"
-        />
-       <GeoJSON data={this.props.data} />
+        />       
+
       </Map>
+
     )
   }
 }
