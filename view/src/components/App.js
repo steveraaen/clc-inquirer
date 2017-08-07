a360bbbefda4dff993df31bb2b359e2a32932080
steveraaen <sraaen@gmail.com>
2017-08-07 17:23:04 -0400

    Left off working on handleClick in Form.js

--- view/src/components/App.js
+++ view/src/components/App.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import CMarkers from './CMarkers.js'
 import Map from './CMarkers.js'
 import Form from './Form.js'
-
 import helpers from './utils/helpers.js'
 import day from './utils/time.js'
-console.log(day);
+
 class App extends Component {
 	constructor(props) {
 		super(props);
     this.state = {
       data: null,
       hoodNames: null,
-      uloc: null
+      uloc: null,
+      dow: day
     }
+
   }
   componentWillMount() {
     navigator.geolocation.getCurrentPosition(function(pos){
@@ -25,25 +26,8 @@ class App extends Component {
     this.setState({ uloc: userLoc})
         }.bind(this))
 
-/*    helpers.initHood().then(function(response) {
-      console.log(response.data[107].name);
-      if (response !== this.state.hood) {
-        var hoodCoorA = response.data[107].geometry.coordinates[0];
-        var hoodName =  response.data[107].name
-        hoodCoorA.pop()
-        console.log(hoodCoorA)
-        var hoodCoorB = hoodCoorA.map((positions, idx) => {
-          return positions
-        })
-        console.log(hoodCoorB)
-        this.setState({ latlngs: hoodCoorB,
-                        hoodList: response.data,
-                        hoodName: hoodName });
-      }
-    }.bind(this))*/
 // ------------
 helpers.getHoodNames().then(function(res){
-
           this.setState({hoodNames: res.data });
 }.bind(this))
 // -----------------------------------------------------
@@ -68,12 +52,18 @@ helpers.getHoodNames().then(function(res){
         })
       }.bind(this))
 }
+/*  setDay(value) {
+    this.setState = ({
+      dayow: this.state.dow
+    })
+  } */
+
   render() {
     
     return (
       <div className="App">
        <div className="header">
-        <Form hoodName={this.state.hoodNames}/>
+        <Form hoodName={this.state.hoodNames}  />
        </div>
         <CMarkers keys={this.state.keys} uloc={this.state.uloc} data={this.state.data} text={this.state.text} hoodName={this.state.hoodName} latlngs={this.state.latlngs}/>
 
--- view/src/components/Form.js
+++ view/src/components/Form.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import helpers from './utils/helpers.js'
 
 export default class Form extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      dayow: ""
+    }
+    this.handleClick = this.handleClick.bind(this)
 }
   componentWillReceiveProps(){
-    console.log(this.props.hoodName)
-    
-
-
+    console.log("will receive props")
   }
+  handleClick(e) {
+    console.log('handledClick')
+    console.log(e.target.value)
+    if(e.target.value === "MON") {
+      helpers.monData()
+    } else {console.log('not monday')}
+    }
   render() {
     if(this.props.hoodName) {
       var hn = this.props.hoodName
@@ -23,13 +32,13 @@ export default class Form extends Component {
       {hoodList}
       </select>
         <input></input>
-        <button className="btn btn-default" type="button"> Lookup </button>
-        <button className="btn btn-default" type="button"> Monday  </button>
-        <button className="btn btn-default" type="button"> Tuesday  </button>
-        <button className="btn btn-default" type="button"> Wednesday  </button>
-        <button className="btn btn-default" type="button"> Thursday  </button>
-        <button className="btn btn-default" type="button"> Friday  </button>
-        <button className="btn btn-default" type="button"> Saturday  </button>
+        <button className="btn btn-default" > Lookup </button>
+        <button className="btn btn-default"  type="submit" id="mon" value="MON" onClick={this.handleClick}> Monday  </button>
+        <button className="btn btn-default"  value="TUE" onClick={this.handleClick}> Tuesday  </button>
+        <button className="btn btn-default"  value="WED" onClick={this.handleClick}> Wednesday  </button>
+        <button className="btn btn-default"  value="THU" onClick={this.handleClick}> Thursday  </button>
+        <button className="btn btn-default"  value="FRI" onClick={this.handleClick}> Friday  </button>
+        <button className="btn btn-default"  value="SAT" onClick={this.handleClick}> Saturday  </button>
           
       </div>
     )
